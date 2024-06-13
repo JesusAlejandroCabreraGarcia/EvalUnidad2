82503ba88998c6f74ba6967e29bd060e48cd8c3c
alejandro <alejandroxlllc@gmail.com>
2024-06-13 11:07:26 -0600

    Agregamos el componente pie de pagina al dashboard

--- frontend/src/components/Dashboard.vue
+++ frontend/src/components/Dashboard.vue
@@ -250,7 +250,7 @@
       </div>
    </aside>
    
- <div class="p-4 sm:ml-64">
+ <!--<div class="p-4 sm:ml-64">
       <div class="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700">
          <div class="grid grid-cols-3 gap-4 mb-4">
             <div class="flex items-center justify-center h-24 rounded bg-gray-50 dark:bg-gray-800">
@@ -350,6 +350,15 @@
             </div>
          </div>
       </div>
-   </div> 
-
+   </div> -->
+<Piepagina></Piepagina>
    </template>
+
+<script>
+import Piepagina from './Piepagina.vue'
+export default {
+    components: { 
+      Piepagina
+    }
+}
+</script>
